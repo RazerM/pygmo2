8d1f2d3c11b977c266bcaf7507acbba5da36e3cf
darioizzo <dario.izzo@gmail.com>
2026-04-11 09:20:04 +0000

    Deploying to gh-pages from @ esa/pygmo2@6e514b7508706c27f997e40898574428bde9b813 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.19.6',
+    VERSION: '2.19.8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
